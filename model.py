39a05e898d2c1c6f42e35adade435da729660cb7
gamer102345 <halimibrahim769@gmail.com>
2025-08-31 14:54:55 -0400

    massive reconstruction and simplification

--- model.py
+++ model.py
@@ -31,32 +31,41 @@ stock_analysis = Stock_module()
 
 #Reading Ticker File to Get Ticker
 i = 0
-with open(file='Tickers.txt',mode='r') as f:
-    tickers = f.readlines()
-    for ticker in tickers:
-        try:
-            #getting data from yahoofinance api
-            ticker_data = yf.Ticker(ticker=ticker).history(period='max').reset_index()
-            dates = ticker_data['Date'].dt.date.apply(lambda x: x.toordinal())
-            price = ticker_data['Close']
+# with open(file='Tickers.txt',mode='r') as f:
+#     tickers = f.readlines()
+#     for ticker in tickers:
+#         try:
+#             #getting data from yahoofinance api
+#             ticker_data = yf.Ticker(ticker=ticker).history(period='max').reset_index()
+#             dates = ticker_data['Date'].dt.date.apply(lambda x: x.toordinal())
+#             price = ticker_data['Close']
 
-            X = torch.tensor(dates.values, dtype=torch.float32).unsqueeze(1)
-            y = torch.tensor(price.values, dtype=torch.float32).unsqueeze(1)
+#             X = torch.tensor(dates.values, dtype=torch.float32).unsqueeze(1)
+#             y = torch.tensor(price.values, dtype=torch.float32).unsqueeze(1)
 
-            #split the data into training and testing data
-            X.numpy()
-            y.numpy()
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, train_size=.8, random_state=42)
+#             #split the data into training and testing data
+#             X.numpy()
+#             y.numpy()
+#             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, train_size=.8, random_state=42)
 
-            combined_train_tensor = [i, X_train, y_train]
-            combined_test_tensor = [i, X_test, y_test]
-            i += 1
-            torch.save(combined_train_tensor, 'train_tensors.pt')
-            torch.save(combined_test_tensor, 'test_tensors.pt')
-        except:
-            print(f'{ticker} is not available')
+#             combined_train_tensor = [i, X_train, y_train]
+#             combined_test_tensor = [i, X_test, y_test]
+#             i += 1
+#             torch.save(combined_train_tensor, 'train_tensors.pt')
+#             torch.save(combined_test_tensor, 'test_tensors.pt')
+#         except:
+#             print(f'{ticker} is not available')
 
+#I will use specific tickers because it is more efficient and faster however the code above does allow for multiple different
+#Tickers they just are not all available and that takes time and computing power
+Ticker = str(input('What Stock would you like to analyze (Ticker): '))
+ticker_data = yf.Ticker(ticker=Ticker).history(period='max').reset_index()
+dates = ticker_data['Date'].dt.date.apply(lambda x: x.toordinal())
+price = ticker_data['Close']
 
+X = torch.tensor(dates.values, dtype=torch.float32).unsqueeze(1)
+y = torch.tensor(price.values, dtype=torch.float32).unsqueeze(1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, train_size=.8, random_state=42)
 
 #Training and Testing
 loss_fn = nn.CrossEntropyLoss()
@@ -65,9 +74,9 @@ optimizer = torch.optim.SGD(stock_analysis.parameters(), lr = .00001)
 epochs = 1000
 for epoch in range(epochs):
     stock_analysis.train()
-    y_logits = stock_analysis(torch.load('train_tensors.pt')[1])
+    y_logits = stock_analysis(X_train)
 
-    loss = loss_fn(y_logits, torch.load('test_tensors.pt')[2])
+    loss = loss_fn(y_logits, y_train)
 
     optimizer.zero_grad()
 
@@ -75,9 +84,9 @@ for epoch in range(epochs):
 
     stock_analysis.eval()
     with torch.inference_mode():
-        test_logits = stock_analysis(torch.load('test_tensors,pt')[1])
+        test_logits = stock_analysis(X_test)
 
-        test_loss = loss_fn(test_logits, torch.load('test_tensors.pt')[2])
+        test_loss = loss_fn(test_logits, y_test)
     
     if epoch % 10 == 0:
         print(f'Epoch: {epoch}/Loss: {loss}/Test loss: {test_loss}')
